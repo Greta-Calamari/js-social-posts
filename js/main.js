a9e3fb2b7685648cef6f55872445d9fd0be1819f
ladykakki <greta_calamari@hotmail.it>
2022-04-12 15:34:28 +0200

    funzione stampa card con for each

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// **Milestone 1** - Creiamo il nostro array di oggetti che rappresentano ciascun post.
+// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+// - id del post, numero progressivo da 1 a n
+// - nome autore,
+// - foto autore,
+// - data in formato americano (mm-gg-yyyy),
+// - testo del post,
+// - immagine (non tutti i post devono avere una immagine),
+// - numero di likes.
 const posts = [
     {
         "id": 1,
@@ -57,18 +66,94 @@ const posts = [
 ];
 
 
-// **Milestone 1** - Creiamo il nostro array di oggetti che rappresentano ciascun post.
-// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
-// - id del post, numero progressivo da 1 a n
-// - nome autore,
-// - foto autore,
-// - data in formato americano (mm-gg-yyyy),
-// - testo del post,
-// - immagine (non tutti i post devono avere una immagine),
-// - numero di likes.
-// *Non è necessario creare date casuali*
-// *Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)*
 // **Milestone 2** - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+// devo stampare questo codice nell'html
+
+{/* <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">Phil Mangione</div>
+                        <div class="post-meta__time">4 mesi fa</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src="https://unsplash.it/600/300?image=171" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+
+ */}
+
+
+// come lo stampo? faccio una funzione..
+// definisco a cosa devo attaccare le card
+const container =document.getElementById('container');
+// console.log(container)
+function stampaCard(){
+    posts.forEach((element) => {
+        let card = document.createElement('div');
+        card.setAttribute('class', 'post');
+        // <div class="cs_card">
+        //     <i style = "color: ${element.color};" class="${element.prefix}solid ${element.prefix}${element.name}"></i>
+        //     <p>${element.name.toUpperCase()}</p>
+        // </div>
+        card.innerHTML = `
+        <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${element.media}" alt="${element.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">4 mesi fa</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src="https://unsplash.it/600/300?image=171" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                    </div>
+                </div> 
+            </div> 
+        
+        
+        
+        `;
+        container.append(card);
+})
+};
+
+stampaCard();
+
 // **Milestone 3** - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 // Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 // ****BONUS**
@@ -84,4 +169,3 @@ const posts = [
 // > console.log() è nostro amico.
 // > Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
 //  Buon lavoro! 
-
